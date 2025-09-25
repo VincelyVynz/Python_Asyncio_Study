31626eb04c16682123e450afdae38bf9dc7c4b63
Vynz <76029153+VincelyVynz@users.noreply.github.com>
2025-09-25 12:09:56 +0530

    Add more url to concurrent_file_downloader.py

--- concurrent_file_downloader.py
+++ concurrent_file_downloader.py
@@ -3,7 +3,8 @@ import asyncio, aiohttp,os
 urls = ["https://images.pexels.com/photos/31001122/pexels-photo-31001122.jpeg",
         "https://images.pexels.com/photos/32838311/pexels-photo-32838311.jpeg",
         "https://images.pexels.com/photos/6415078/pexels-photo-6415078.jpeg",
-        "https://www.pexels.com/download/video/1583096.mp4"]
+        "https://www.pexels.com/download/video/1583096.mp4",
+        "https://www.pexels.com/download/video/9885661.mp4"]
 
 # Sample images and video URLs used in this project are from Pexels (https://www.pexels.com)
 
